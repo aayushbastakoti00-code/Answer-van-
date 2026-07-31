727235d0158771278a9403a898411521c25333a2
aayushbastakoti00-code <aayushbastakoti00@gmail.com>
2026-07-31 14:40:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 <script src="script.js"></script>
+{
+ image: "photo1.jpg",
+ question: "If we both were animals, k timi ma sanga milera bari ko ghas khantheu? 😙🥹",
+ options: [
+  "Yes 🥹",
+  "No 😠",
+  "Malai ghas manpardaina 🤮",
+  "Nai timilai herera basthey 😴"
+ ],
+ answer: 0
+}
